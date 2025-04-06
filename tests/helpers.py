8d9909263fa2fab20bbffb585def4bb6bf43f43a
Vlad <bagirov.vladislav@ya.ru>
2025-04-06 10:30:16 +0400

    my home work in 5 sprint v1.3

--- tests/helpers.py
+++ tests/helpers.py
@@ -3,13 +3,10 @@ import random
 
 def login_random():
     three_number = random.randint(100, 999)
-    #login_random = f'VladB_20{three_number}@ya.ru'
     return f'VladB_20{three_number}@ya.ru'
 
 def password_6_symbol():
-    #password_6_symbol = random.randint(100000, 999999)
     return random.randint(100000, 999999)
 
 def password_3_symbol():
-    # password_3_symbol = random.randint(100, 999)
     return random.randint(100, 999)
